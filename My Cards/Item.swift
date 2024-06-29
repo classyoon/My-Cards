305d7207b52ab602e2a62bb2087fbe99a6903cbf
classyoon <96508736+classyoon@users.noreply.github.com>
2024-06-29 12:43:34 -0400

    Initial Commit

---
+++ My Cards/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  My Cards
+//
+//  Created by Conner Yoon on 6/29/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
